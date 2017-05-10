3604bfeaf638ad6296a8a18110d9cf026d56e1fb
Pascal Cudré <pascal.cudre@liip.ch>
2017-05-10 09:45:13 +0200

    Adapt deprecated fragment attach method

--- mobile/src/main/java/ch/liip/timeforcoffee/fragment/DepartureListFragment.java
+++ mobile/src/main/java/ch/liip/timeforcoffee/fragment/DepartureListFragment.java
@@ -1,6 +1,6 @@
 package ch.liip.timeforcoffee.fragment;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ListFragment;
@@ -58,12 +58,12 @@ public class DepartureListFragment extends ListFragment implements SwipeRefreshL
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        if (!(activity instanceof Callbacks)) {
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (!(context instanceof Callbacks)) {
             throw new IllegalStateException("Activity must implement fragment's callbacks.");
         }
-        mCallbacks = (Callbacks) activity;
+        mCallbacks = (Callbacks) context;
     }
 
     @Override
