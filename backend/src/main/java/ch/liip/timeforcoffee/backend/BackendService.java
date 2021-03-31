acaa1800fde9288ec2054f96524ca567d1575181
Nicolas Dougoud <nicolas.dougoud@liip.ch>
2021-03-31 13:43:44 +0200

    connection api

--- backend/src/main/java/ch/liip/timeforcoffee/backend/BackendService.java
+++ backend/src/main/java/ch/liip/timeforcoffee/backend/BackendService.java
@@ -5,16 +5,21 @@ import java.util.Map;
 
 import retrofit.http.GET;
 import retrofit.http.Headers;
+import retrofit.http.Path;
 import retrofit.http.QueryMap;
 import rx.Observable;
 
 public interface BackendService {
 
     @Headers("Cache-Control:public, max-age=20")
-    @GET("/api/ch/stationboard")
-    Observable<List<Departure>> getDepartures(@QueryMap Map<String, String> query);
+    @GET("/api/ch/stationboard/{stationId}")
+    Observable<Departures> getDepartures(@Path("stationId") String stationId);
 
     @Headers("Cache-Control:public, max-age=20")
-    @GET("/api/ch/connections")
-    Observable<List<Connection>> getConnections(@QueryMap Map<String, String> query);
+    @GET("/api/ch/connections/{stationId}/{destinationId}/{departureDate}/{arrivalDate}")
+    Observable<Connections> getConnections(
+            @Path("stationId") String stationId,
+            @Path("destinationId") String destinationId,
+            @Path("departureDate") String departureDate,
+            @Path("arrivalDate") String arrivalDate);
 }
