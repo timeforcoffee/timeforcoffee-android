10888281546322cd6ced0e83fe143c6fe16e8c47
Nicolas Dougoud <nicolas.dougoud@liip.ch>
2021-04-05 11:15:59 +0200

    remove outdated library

--- api/src/main/java/ch/liip/timeforcoffee/api/models/Station.java
+++ api/src/main/java/ch/liip/timeforcoffee/api/models/Station.java
@@ -2,16 +2,12 @@ package ch.liip.timeforcoffee.api.models;
 
 import android.location.Location;
 
-import com.directions.route.Route;
-import com.directions.route.RoutingListener;
-import com.google.android.gms.maps.model.PolylineOptions;
+public class Station {
 
-public class Station implements RoutingListener {
-
-    private int id;
-    private String name;
-    private Location location;
-    private float distance;
+    private final int id;
+    private final String name;
+    private final Location location;
+    private final float distance;
     private boolean isFavorite;
 
     public Station(int id, String name, float distance, Location location, boolean isFavorite) {
@@ -69,28 +65,10 @@ public class Station implements RoutingListener {
     }
 
     @Override
-    public void onRoutingFailure() {
-    }
-
-    @Override
-    public void onRoutingStart() {
-
-    }
-
-    @Override
-    public void onRoutingSuccess(PolylineOptions mPolyOptions, Route route) {
-    }
-
-    @Override
-    public void onRoutingCancelled() {
-
-    }
-
-    @Override
     public boolean equals(Object object) {
         boolean sameName = false;
 
-        if (object != null && object instanceof Station) {
+        if (object instanceof Station) {
             sameName = this.name.equals(((Station) object).getName());
         }
 
